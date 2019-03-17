a4331601cb1aa4e080b5479231fcfa3764cfbe2b
poteminr <poteminr@gmail.com>
2019-03-17 08:58:44 -0400

    add example for text_preproc

--- NLP/train_nlp.py
+++ NLP/train_nlp.py
@@ -1,74 +1,71 @@
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import f1_score,accuracy_score
-import sklearn
-from sklearn.feature_extraction.text import TfidfVectorizer
 import pandas as pd
-import numpy as np
 import scipy as sc
 import nltk
 from nltk.corpus import stopwords
 import re
 from nltk.tokenize import RegexpTokenizer
-#TODO: Убрать стоп-слова
-# Убрать символы !n числам или словам
-# Перенести импорты в начало файла
-# Запилить класс для препроцессинга текста(для финала)
-# Протестить склерн модельки и запилить нейронку для классификации
-# Заюзать self-attention для текста(вроде круто работает)
-# Оформить нормально файл
-stopWords = set(stopwords.words('english'))
-
-example = 'All work and no play makes jack dull boy. All work and no play makes jack a dull boy.'
-df = pd.read_csv('NLP/train.csv')
-df = df.drop('qid',axis=1)
-
-#train , test , y_train, y_test = train_test_split(df.question_text,df.target,test_size=0.3)
-
-
-
-def standardize_text(df, text_field = 'question_text'):
-    df[text_field] = df[text_field].str.replace(r"http\S+", "")
-    df[text_field] = df[text_field].str.replace(r"http", "")
-    df[text_field] = df[text_field].str.replace(r"@\S+", "")
-    df[text_field] = df[text_field].str.replace(r"[^A-Za-z0-9(),!?@\'\`\"\_\n]", " ")
-    df[text_field] = df[text_field].str.replace(r"@", "at")
-    df[text_field] = df[text_field].str.lower()
-    return df
-
-def delete_stopw(token_list):
-    """
-    Удаляем стоп-слова из листа после токенизации
-    """
-    clear_text = []
-    for word in token_list:
-        if word not in stopWords:
-            clear_text.append(word)
-    return clear_text
-
-
-
-dataset = standardize_text(df)
-tokenizer = RegexpTokenizer(r'\w+')
-
-dataset.question_text = dataset.question_text.apply(tokenizer.tokenize)
-dataset.question_text = dataset.question_text.apply(lambda x: delete_stopw(x))
-
 from nltk.stem.snowball import SnowballStemmer
 
-stemmer = SnowballStemmer("english")
-stemmer.stem('romans')
 
-
-ex = ['dogs','romans','kills']
-
-def stemming(token_list):
+class TextPreproc(object):
     """
-    Просто проходим по токенам
-    и аппендим чистые в новый лист
+    Класс для препроцессинга текста
     """
-    stemmed_list = []
-    for word in token_list:
-        stemmed_list.append(stemmer.stem(word))
-    return stemmed_list
+    def __init__(self,language='english'):
+        self.stopWords = set(stopwords.words(language))
+        self.tokenizer = RegexpTokenizer(r'\w+')
+        self.stemmer = SnowballStemmer(language)
 
-dataset.question_text = dataset.question_text.apply(lambda x:stemming(x))
+    def standardize_text(self,df, column_name):
+        """
+        Удаляем лишние символы из текста
+        """
+        df[column_name] = df[column_name].str.replace(r"http\S+", "")
+        df[column_name] = df[column_name].str.replace(r"http", "")
+        df[column_name] = df[column_name].str.replace(r"@\S+", "")
+        df[column_name] = df[column_name].str.replace(r"[^A-Za-z0-9(),!?@\'\`\"\_\n]", " ")
+        df[column_name] = df[column_name].str.replace(r"@", "at")
+        df[column_name] = df[column_name].str.lower()
+        return df
+
+    def tokinizer(self,df,column_name):
+        """
+        Преобразуем строки текста из pandas-столбика в лист с токенами
+        """
+        df[column_name] = df[column_name].apply(self.tokenizer.tokenize)
+        return df
+
+
+    def delete_stopw(self,token_list):
+        """
+        Удаляем стоп-слова из листа после токенизации
+        """
+        clear_text = []
+        for word in token_list:
+            if word not in self.stopWords:
+                clear_text.append(word)
+        return clear_text
+
+    def stemming(self,token_list):
+        """
+        Просто проходим по токенам
+        и аппендим чистые в новый лист
+        """
+        stemmed_list = []
+        for word in token_list:
+            stemmed_list.append(self.stemmer.stem(word))
+        return stemmed_list
+#
+#Example
+# df = pd.read_csv('NLP/train.csv')
+# df = df.drop('qid',axis=1)
+#
+# txt_preproc = TextPreproc(language='english')
+#
+# df = txt_preproc.standardize_text(df=df,column_name='question_text')
+#
+# df = txt_preproc.tokinizer(df=df,column_name='question_text')
+#
+# df['question_text'] = df['question_text'].apply(lambda x: txt_preproc.delete_stopw(x))
+#
+# df['question_text'] = df['question_text'].apply(lambda x: txt_preproc.stemming(x))
